e96ba71237d02b5f4bedc0fc3435bf90070d2435
JOAO GOMES <crijoaogomes@gmail.com>
2026-07-22 03:20:33 +0000

    feat: trip fact model

---
+++ models/trip_fact.sql
@@ -0,0 +1,14 @@
+{{config(materialized='table')}}
+
+with trips as(
+    select
+        BIKE_ID,
+        DATE(TO_TIMESTAMP(START_TIME)) AS TRIP_DATE,
+        START_STATION_ID,
+        END_STATION_ID,
+        USER_TYPE,
+        TIMESTAMPDIFF(second, TO_TIMESTAMP(START_TIME), TO_TIMESTAMP(STOPTIME)) as TRIP_DURATION
+    from {{source('demo', 'BIKES')}}
+    LIMIT 10
+)
+SELECT * FROM trips
